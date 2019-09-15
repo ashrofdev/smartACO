1b0f9b77c20f837b4da8af85c8ac5d88390fda57
First Last <salmanashraf853@gmail.com>
2019-09-15 08:11:15 +0100

    improving responsiveness

--- src/components/Content.js
+++ src/components/Content.js
@@ -3,11 +3,13 @@ import React from 'react'
 const Content = ({msg}) => {
     return (
         <div className="cont">
+            <div>
             <h1>SMARTACO</h1>
             <div className="request">
                 <input placeholder="Chat with smarta..."/>
+                <div className="smarta">{msg}</div>
             </div>
-            <div className="smarta">{msg}</div>
+            </div>
             <img src={require('../g.gif')} alt="" />
         </div>
     )
