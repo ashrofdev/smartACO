f4a8b3342f7d92305558687d773bcb6fcf0b5344
First Last <salmanashraf853@gmail.com>
2019-10-10 23:10:25 +0100

    Updates

---
+++ precache-manifest.204a5dd142640b5459c952bfbdc22d7e.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8b43de7354c657356fb3aec773629be",
+    "url": "/smartACO/index.html"
+  },
+  {
+    "revision": "68866a0d4e9fac13c586",
+    "url": "/smartACO/static/css/2.aaf86575.chunk.css"
+  },
+  {
+    "revision": "b1e3b8da3396569aa43e",
+    "url": "/smartACO/static/css/main.f26e0b95.chunk.css"
+  },
+  {
+    "revision": "68866a0d4e9fac13c586",
+    "url": "/smartACO/static/js/2.40a8b625.chunk.js"
+  },
+  {
+    "revision": "b1e3b8da3396569aa43e",
+    "url": "/smartACO/static/js/main.794bbff9.chunk.js"
+  },
+  {
+    "revision": "7b32b0030dde31a941ff",
+    "url": "/smartACO/static/js/runtime~main.c37d4d34.js"
+  },
+  {
+    "revision": "ca0c81aff6b6a0d5f257f165ffbf7626",
+    "url": "/smartACO/static/media/ai2.ca0c81af.jpeg"
+  },
+  {
+    "revision": "1864f18342eb2699a9d9d550d7bec0fa",
+    "url": "/smartACO/static/media/ai3.1864f183.jpeg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/smartACO/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/smartACO/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/smartACO/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/smartACO/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/smartACO/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "0c194e0388774f957906aa97d5d0be50",
+    "url": "/smartACO/static/media/g.0c194e03.gif"
+  },
+  {
+    "revision": "666f1c8cc46426f8a7f7f0cae9ca9bc4",
+    "url": "/smartACO/static/media/smartaco.666f1c8c.gif"
+  }
+]);
